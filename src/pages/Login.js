2777537cc03eeb1ae58ade55fc0f34bdb18cd71d
Ester Viana Verza <ester.av@hotmail.com>
2022-09-22 13:55:00 -0300

    req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,48 +1,69 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
+  state = {
+    nameLogin: '',
+    isLoading: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  loginUser = async () => {
+    const { nameLogin } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name: nameLogin });
+    history.push('/search');
+  };
+
   render() {
-    const { nameLogin,
-      handleChange,
-      loginButtonDisabled,
-      loginUser } = this.props;
+    const { nameLogin, isLoading } = this.state;
+    const minLogin = 2;
 
     return (
       <div data-testid="page-login">
-        <label htmlFor="nameLogin">
-          Username
-          <input
-            type="text"
-            value={ nameLogin }
-            name="nameLogin"
-            onChange={ handleChange }
-            data-testid="login-name-input"
-          />
-        </label>
+        { isLoading
+          ? <Loading />
+          : (
+            <div>
+              <label htmlFor="nameLogin">
+                Username
+                <input
+                  type="text"
+                  value={ nameLogin }
+                  name="nameLogin"
+                  onChange={ this.handleChange }
+                  data-testid="login-name-input"
+                />
+              </label>
 
-        <button
-          type="submit"
-          name="login"
-          data-testid="login-submit-button"
-          disabled={ loginButtonDisabled }
-          onClick={ loginUser }
-        >
-          Entrar
-        </button>
-
+              <button
+                type="submit"
+                name="login"
+                data-testid="login-submit-button"
+                disabled={ nameLogin.length <= minLogin }
+                onClick={ this.loginUser }
+              >
+                Entrar
+              </button>
+            </div>
+          )}
       </div>
     );
   }
 }
 
 Login.propTypes = {
-  nameLogin: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired,
-  loginButtonDisabled: PropTypes.bool.isRequired,
-  loginUser: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 };
-
-/* loading: PropTypes.bool.isRequired,
-userName: PropTypes.string.isRequired,
-savedUserName: PropTypes.bool.isRequired, */
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 export default class Search extends Component {
   render() {
-    const { userName } = this.props;
     return (
       <div data-testid="page-search">
         <h1>Search</h1>
-        <p>
-          {' '}
-          logado como
-          {' '}
-          {userName}
-          {' '}
-        </p>
       </div>
     );
   }
 }
-
-Search.propTypes = {
-  userName: PropTypes.string.isRequired,
-};

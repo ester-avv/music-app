0ef81314a7b65199d422153a0b1d5b2a02994897
Ester Viana Verza <ester.av@hotmail.com>
2022-09-27 15:09:59 -0300

    alteração profile e favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
 
 export default class Favorites extends Component {
+  state = {
+    favorites: [],
+    songs: [],
+  };
+
+  async componentDidMount() {
+    const myFavorites = await getFavoriteSongs();
+    this.setState({ favorites: myFavorites,
+      songs: myFavorites });
+  }
+
   render() {
+    const { favorites, songs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        <h1>Favorites</h1>
+        <div>
+          {favorites
+            ? (
+              <section>
+                <h1>Músicas Favoritas</h1>
+                <MusicCard
+                  songs={ songs }
+                />
+              </section>)
+            : <Loading />}
+        </div>
       </div>
     );
   }

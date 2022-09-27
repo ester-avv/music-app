006568d950372d42a23f7f5970e4e4627e7a7737
Ester Viana Verza <ester.av@hotmail.com>
2022-09-26 21:04:20 -0300

    req 7

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -2,23 +2,80 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class MusicCard extends Component {
-  render() {
+  state = {
+    mySongs: [],
+  };
+
+  componentDidMount() {
+    this.fixState();
+  }
+
+  fixState = () => {
     const { songs } = this.props;
-    console.log(songs);
+    this.setState({ mySongs: songs });
+  };
+
+  render() {
+    const { mySongs } = this.state;
     return (
-      <section data-testid="artist-name">
-        {songs.map((element) => (
-          <div key={ element.trackId }>
+      <div>
+        { mySongs.map((e) => (
+          <div key={ e.trackId }>
             <h3>
-              { element.trackName }
+              {' '}
+              {e.trackName}
             </h3>
+            <audio data-testid="audio-component" src="{e.previewUrl}" controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
           </div>
         ))}
-      </section>
+      </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  songs: PropTypes.any.isRequired,
+  songs: PropTypes.shape({
+    artistId: PropTypes.string,
+    artistName: PropTypes.string,
+    artistViewUrl: PropTypes.string,
+    artworkUrl30: PropTypes.string,
+    artworkUrl60: PropTypes.string,
+    artworkUrl100: PropTypes.string,
+    collectionCensoredName: PropTypes.string,
+    collectionExplicitness: PropTypes.string,
+    collectionId: PropTypes.number,
+    collectionName: PropTypes.string,
+    collectionPrice: PropTypes.number,
+    collectionViewUrl: PropTypes.string,
+    country: PropTypes.string,
+    currency: PropTypes.string,
+    discCount: PropTypes.number,
+    discNumber: PropTypes.number,
+    isStreamable: PropTypes.bool,
+    kind: PropTypes.string,
+    previewUrl: PropTypes.string,
+    primaryGenreName: PropTypes.string,
+    releaseDate: PropTypes.string,
+    trackCensoredName: PropTypes.string,
+    trackCount: PropTypes.number,
+    trackExplicitness: PropTypes.string,
+    trackId: PropTypes.number,
+    trackName: PropTypes.string,
+    trackNumber: PropTypes.number,
+    trackPrice: PropTypes.number,
+    trackTimeMillis: PropTypes.number,
+    trackViewUrl: PropTypes.string,
+    wrapperType: PropTypes.string,
+  }).isRequired,
 };
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,19 +3,25 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import Loading from '../components/Loading';
 /* import BrowserRouter from 'react-router-dom'; */
 
 export default class Album extends Component {
   state = {
     songs: [],
     myAlbum: '',
+    isLoading: false,
   };
 
   async componentDidMount() {
+    this.setState({ isLoading: true });
     this.getCollectionId();
     const myAlbumSongs = await this.getAlbumSongs();
+    const mySongs = await myAlbumSongs.filter((_element, index) => index > 0);
+    console.log('tudo', myAlbumSongs);
+    console.log('só as musicas', mySongs);
     /* const allSongs = await myAlbumSongs; */
-    this.setState({ songs: myAlbumSongs, myAlbum: myAlbumSongs[0] });
+    this.setState({ songs: mySongs, myAlbum: myAlbumSongs[0], isLoading: false });
   }
 
   getCollectionId = () => {
@@ -36,25 +42,31 @@ export default class Album extends Component {
 
   render() {
     /* console.log(this.props); */
-    const { songs, myAlbum } = this.state;
+    const { songs, myAlbum, isLoading } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <section>
-          <div>
-            <h1 data-testid="artist-name">
-              {' '}
-              { myAlbum.artistName }
-              {' '}
-            </h1>
-            <h2 data-testid="album-name">
-              { myAlbum.collectionName }
-            </h2>
-          </div>
-        </section>
-        <MusicCard
-          songs={ songs }
-        />
+        { isLoading
+          ? <Loading />
+          : (
+            <div>
+              <section>
+                <div>
+                  <h1 data-testid="artist-name">
+                    {' '}
+                    { myAlbum.artistName }
+                    {' '}
+                  </h1>
+                  <h2 data-testid="album-name">
+                    { myAlbum.collectionName }
+                  </h2>
+                </div>
+              </section>
+              <MusicCard
+                songs={ songs }
+              />
+            </div>
+          )}
       </div>
     );
   }
